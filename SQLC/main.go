20a9447670a0f9af70dd1756c6f76c8bccef6c42
kaungkhantcoder <kaungkhantcoder@gmail.com>
2025-10-22 23:41:12 +0630

    sqlc

--- SQLC/main.go
+++ SQLC/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
 
 func main() {
-	fmt.Println("Hello World")
+	connStr := "postgres://postgres::@localhost:5432/sqlctest?sslmode=disabled"	
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 }
