7bf1c094c4d03ea1f9433ea248bc1dd15cc9b982
kaungkhantcoder <kaungkhantcoder@gmail.com>
2025-10-24 23:11:56 +0630

    goBasic

---
+++ Golang_Basic/function.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func plus(a int, b int) int {
+	return a + b
+}
+
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+
+func main() {
+	res := plus(5, 10)
+	fmt.Println(res)
+
+	res = plusPlus(5, 10, 15)
+	fmt.Println(res)
+}
