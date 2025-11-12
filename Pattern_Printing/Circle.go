8ce912757bf62d6ee4356538b8b227463250f0e4
kaungkhantcoder <kaungkhantcoder@gmail.com>
2025-11-12 23:31:39 +0630

    Circle Shape

---
+++ Pattern_Printing/Circle.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Circle() {
+	const radius = 10.0
+	const tolerance = 0.7
+
+	centerX := radius
+	centerY := radius
+
+	for y := 0.0; y <= 2*radius; y++ {
+
+		for x := 0.0; x <= 2*radius; x++ {
+
+			dx := x - centerX
+			dy := y - centerY
+
+			distance := math.Sqrt(dx*dx + dy*dy)
+
+
+			if math.Abs(distance - radius) < tolerance {
+				fmt.Print("● ")
+			} else {
+				fmt.Print("  ")
+			}
+		}
+		fmt.Println()
+	}
+}
--- Pattern_Printing/main.go
+++ Pattern_Printing/main.go
@@ -1,10 +1,11 @@
 package main
 
 func main() {
-	R_Triangle()
-	Pyramid()
-	Diamond()
-	HollowSquare()
-	ReverseTriangle()
-	Butterfly()
+	// R_Triangle()
+	// Pyramid()
+	// Diamond()
+	// HollowSquare()
+	// ReverseTriangle()
+	// Butterfly()
+	Circle()
 }
