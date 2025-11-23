16fb1de78378ed5a19dae097ecba5f0553dba6f5
kaungkhantcoder <kaungkhantcoder@gmail.com>
2025-11-23 23:38:52 +0630

    heart

---
+++ Pattern_Printing/Heart.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func PrintHeartPattern(size int) {
+
+	if size%2 == 0 {
+		size++
+	}
+
+	for row := size / 2; row <= size; row += 2 {
+
+		for col := 1; col < size-row; col += 2 {
+			fmt.Print(" ")
+		}
+
+
+		for col := 1; col <= row; col++ {
+			fmt.Print("*")
+		}
+
+	
+		for col := 1; col <= size-row; col++ {
+			fmt.Print(" ")
+		}
+
+
+		for col := 1; col <= row-1; col++ {
+			fmt.Print("*")
+		}
+
+		fmt.Println() 
+	}
+
+
+	for row := size; row >= 0; row-- {
+
+		for col := row; col < size; col++ {
+			fmt.Print(" ")
+		}
+
+
+		for col := 1; col <= (row*2)-1; col++ {
+			fmt.Print("*")
+		}
+
+		if (row*2)-1 > 0 {
+			fmt.Println()
+		}
+	}
+}
+
+func Heart() {
+
+	heartSize := 10
+	fmt.Printf("Printing a heart pattern with size %d:\n\n", heartSize)
+	PrintHeartPattern(heartSize)
+}
