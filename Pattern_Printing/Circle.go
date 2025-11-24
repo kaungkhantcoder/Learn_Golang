aa96b34cd33c639b6938dbd49ca52553a0453796
kaungkhantcoder <kaungkhantcoder@gmail.com>
2025-11-24 23:16:31 +0630

    error

--- Pattern_Printing/Circle.go
+++ Pattern_Printing/Circle.go
@@ -1,37 +1,3 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"math"
-// )
-
-// func Circle() {
-// 	const radius = 10.0
-// 	const tolerance = 0.7
-
-// 	centerX := radius
-// 	centerY := radius
-
-// 	for y := 0.0; y <= 2*radius; y++ {
-
-// 		for x := 0.0; x <= 2*radius; x++ {
-
-// 			dx := x - centerX
-// 			dy := y - centerY
-
-// 			distance := math.Sqrt(dx*dx + dy*dy)
-
-
-// 			if math.Abs(distance - radius) < tolerance {
-// 				fmt.Print("* ")
-// 			} else {
-// 				fmt.Print("  ")
-// 			}
-// 		}
-// 		fmt.Println()
-// 	}
-// }
-
 package main
 
 import (
@@ -46,16 +12,19 @@ func Circle() {
 	centerX := radius
 	centerY := radius
 
-	for y:= 0.0; y <= 2*radius; y++ {
+	for y := 0.0; y <= 2*radius; y++ {
+
 		for x := 0.0; x <= 2*radius; x++ {
+
 			dx := x - centerX
 			dy := y - centerY
 
 			distance := math.Sqrt(dx*dx + dy*dy)
 
+
 			if math.Abs(distance - radius) < tolerance {
 				fmt.Print("* ")
-			}else {
+			} else {
 				fmt.Print("  ")
 			}
 		}
